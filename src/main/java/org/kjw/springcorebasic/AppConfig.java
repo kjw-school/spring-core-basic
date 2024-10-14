3c7333e1ed3cdcc4c870b6cd513067ab7b31a2a1
kjw1995 <114547095+kjw1995@users.noreply.github.com>
2024-10-14 23:45:19 +0900

    Study Day03

--- src/main/java/org/kjw/springcorebasic/AppConfig.java
+++ src/main/java/org/kjw/springcorebasic/AppConfig.java
@@ -1,6 +1,9 @@
 package org.kjw.springcorebasic;
 
+import org.kjw.springcorebasic.discount.DiscountPolicy;
 import org.kjw.springcorebasic.discount.FixDiscountPolicy;
+import org.kjw.springcorebasic.discount.RateDiscountPolicy;
+import org.kjw.springcorebasic.member.MemberRepository;
 import org.kjw.springcorebasic.member.MemberService;
 import org.kjw.springcorebasic.member.MemberServiceImpl;
 import org.kjw.springcorebasic.member.MemoryMemberRepository;
@@ -10,11 +13,19 @@ import org.kjw.springcorebasic.order.OrderServiceImpl;
 public class AppConfig {
 
     public MemberService memberService() {
-        return new MemberServiceImpl(new MemoryMemberRepository());
+        return new MemberServiceImpl(memberRepository());
+    }
+
+    private MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
     }
 
     public OrderService orderService() {
-        return new OrderServiceImpl(new MemoryMemberRepository(), new FixDiscountPolicy());
+        return new OrderServiceImpl(memberRepository(), discountPolicy());
+    }
+
+    public DiscountPolicy discountPolicy() {
+        return new RateDiscountPolicy();
     }
 
 }
